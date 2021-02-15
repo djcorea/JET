9c5023d87661ec118fdad028c9d414006e51f1ad
djcorea <javier.corea27@gmail.com>
2021-02-15 00:44:06 -0600

    CRUD

--- resources/views/articulo/create.blade.php
+++ resources/views/articulo/create.blade.php
@@ -2,4 +2,26 @@
 
 @section('contenido')
  <h2>CREAR REGISTROS</h2>
+
+<form action="/articulos" method="post">
+    <div class="mb-3">  
+        <label for="" class="form-label">Código</label>
+        <input type="text" name="codigo" id="codigo" class="form-control" tabindex="1">
+    </div>
+
+    <div class="mb-3">  
+        <label for="" class="form-label">Descripción</label>
+        <input type="text" name="descripcion" id="descripcion" class="form-control" tabindex="1">
+    </div>
+
+    <div class="mb-3">  
+        <label for="" class="form-label">cantidad</label>
+        <input type="text" name="cantidad" id="cantidad" class="form-control" tabindex="1">
+    </div>
+
+    <div class="mb-3">  
+        <label for="" class="form-label">Precio</label>
+        <input type="text" name="precio" id="precio" class="form-control" tabindex="1">
+    </div>
+</form>
 @endsection
